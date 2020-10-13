4b35b37c47ca65a551f33e3f6a3aa6aca29158dd
Forgi4G <forgingfilms@gmail.com>
2020-10-13 12:49:11 -0700

    Adding a local framework in order to make sending messages easier, documentation on local-framework.js file.

---
+++ src/utils/local-framework.js
@@ -0,0 +1,74 @@
+class LocalFramework {
+    constructor(options = {}) {
+        this.options = options;
+    }
+
+    /** Send a message and completely ignores all errors to avoid API errors
+     *
+     * @param input
+     */
+    sendMSG(input) {
+        if (this.options.listener) {
+            let listener = this.options.listener;
+            listener.channel.send(input).catch(err => { return err; });
+        } else if (!this.options.listener) {
+            throw new Error("Missing an event listener");
+        } else if (!input) {
+            throw new Error("Missing input");
+        }
+    }
+
+    /** Sends a temporary message and ignores all errors
+     *
+     * @param input - Your content
+     * @param ms - milliseconds
+     */
+    sendTempMSG(input, ms) {
+        // Defaulting the timeout
+        if (!ms) { ms = 3000; }
+        if (this.options.listener) {
+            (this.options.listener).channel.send(input).catch(err => { return err; })
+                .then(m => { if (m) m.delete({ timeout: ms })
+                    .catch(err => { return err; })})
+        } else if (!this.options.listener) {
+            throw new Error("Missing an event listener");
+        } else if (!input) {
+            throw new Error("Missing input");
+        }
+    }
+
+    /** Deletes a message and can delete on a timer
+     *
+     * @param message - required parameter in order to function
+     * @param ms - optional timeout
+     */
+    deleteMSG(message, ms) {
+        if (!message) throw new Error("Missing a message to delete");
+        if (!ms) message.delete();
+        if (ms) {
+            message.delete({ timeout: ms }).catch(err => { return err; })
+        }
+    }
+}
+
+module.exports = LocalFramework;
+
+/*
+How to use:
+
+1.) Require the class in a file
+2.) Initialize the listener object (usually known as message) in the async method as { listener: <message> } in the constructor
+3.) Call it
+4.) See example below if you need a reference
+
+Example:
+run: async (client, message, args) => {
+    const LocalFramework = require("../../utils/local-framework");
+    // Uses the message (2nd parameter) as the listener.
+    const localFramework = new LocalFramework({ listener: message });
+
+    // Send a message with the framework
+    localFramework.sendMSG("Greetings."); // Sends the message to the channel the message is in which is sent as "Greetings."
+}
+
+ */
