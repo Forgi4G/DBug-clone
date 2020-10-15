02bf463648f7e23ba1ca48fc1ea7ee13e2a92333
Forgi4G <forgingfilms@gmail.com>
2020-10-15 09:29:39 -0700

    Code now uses the local framework which drastically makes things easier. There is also a code cleanup which also makes the code more readable. Reminder that !args[0] is equivalent to args[0] === null and args[0] === undefined.

--- src/commands/report/attach.js
+++ src/commands/report/attach.js
@@ -2,6 +2,7 @@ const Report = require("../../models/report");
 const ReportHandler = require("../../handlers/report");
 const Log = require("../../handlers/logging");
 const clc = require("cli-color");
+const LocalFramework = require('../../utils/local-framework');
 
 module.exports = {
 	name: "attach",
@@ -9,23 +10,16 @@ module.exports = {
 	description: "Attach a file to a report",
 	roles: [],
 	run: async (client, message, args) => {
-		message.delete({ timeout: 3000 });
-		if (args[0] === undefined)
-			return message
-				.reply("You forgot the report id")
-				.then((msg) => msg.delete({ timeout: 3000 }));
-		else if (args[1] === undefined)
-			return message
-				.reply("You forgot a url for the attachment")
-				.then((msg) => msg.delete({ timeout: 3000 }));
-		else if (args[2] === undefined)
-			return message
-				.reply("You forgot a name for the attachment")
-				.then((msg) => msg.delete({ timeout: 3000 }));
+		let msgFrame = new LocalFramework({ listener: message });
+		if (message.deletable) msgFrame.deleteMSG(message);
+		if (!args[0])
+			return msgFrame.sendTempReply("You forgot the report ID.");
+		else if (!args[1])
+			return msgFrame.sendTempReply("You forgot an URL for the attachment.");
+		else if (!args[2])
+			return msgFrame.sendTempReply("You forgot a name for the attachment.");
 
-		const id = args[0];
-		const url = args[1];
-		const name = args.slice(2).join(" ");
+		const id = args[0], url = args[1], name = args.slice(2).join(" ");
 
 		const r = async function (params) {
 			try {
@@ -36,14 +30,10 @@ module.exports = {
 		};
 
 		const foundReport = await r({ reportID: id });
-		if (foundReport === null)
-			return message
-				.reply("Hmm that report doesnt exist :(")
-				.then((msg) => msg.delete({ timeout: 3000 }));
+		if (!foundReport)
+			return msgFrame.sendTempReply("That report doesnt exist :(");
 		else if (foundReport.stance === "Denied")
-			return message
-				.reply("This bug has been denied.")
-				.then((msg) => msg.delete({ timeout: 3000 }));
+			return msgFrame.sendTempReply("This bug has already been denied.");
 
 		await Report.updateOne(
 			{ _id: foundReport._id },
@@ -55,15 +45,13 @@ module.exports = {
 			}
 		);
 
-		ReportHandler.UpdateStance(client, await r({ reportID: id }));
+		await ReportHandler.UpdateStance(client, await r({reportID: id}));
 
 		Log.Send(
 			client,
 			`📎 Bug \`\`#${foundReport.reportID}\`\` submitted by ${foundReport.userTag} (${foundReport.userID}) **got a new attachment** attached by **${message.author.username}**#${message.author.discriminator} (${message.author.id})\n**URL:** ${url}\n**Name:** ${name}`
 		);
 
-		return message
-			.reply(`The eagle has landed!`)
-			.then((msg) => msg.delete({ timeout: 3000 }));
+		return msgFrame.sendTempMSG(`The eagle has landed!`);
 	},
 };
